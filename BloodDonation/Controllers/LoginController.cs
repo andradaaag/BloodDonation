e781c5ed43f3211d1e27cc2c253c1215d0550e69
andradaaag <gaeandrada@gmail.com>
2018-04-12 14:30:05 +0300

    Connected SignUp to Login

--- BloodDonation/Controllers/LoginController.cs
+++ BloodDonation/Controllers/LoginController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Antlr.Runtime.Tree;
+using BloodDonation.Models;
 
 namespace BloodDonation.Controllers
 {
@@ -11,6 +12,18 @@ namespace BloodDonation.Controllers
     {
         public ActionResult Index()
         {
+            return View("LoginHomePage");
+        }
+
+        public ActionResult ShowSignUpPage()
+        {
+            return View("SignUpView", new SignUpForm());
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(SignUpForm form)
+        {
+            var firstName = form.FirstName;
             return View("LoginHomePage");
         }
     }
---
+++ BloodDonation/Models/SignUpForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BloodDonation.Models
+{
+    public class SignUpForm
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public DateTime DOB { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String Country { get; set; }
+        public String OtherAddress { get; set; }
+        public String OtherCity { get; set; }
+        public String OtherCountry { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public int UserType { get; set; }
+    }
+}
