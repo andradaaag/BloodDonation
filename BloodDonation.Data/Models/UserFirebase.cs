5c86913a159245b7757a346ffe0b08d6e12cddc3
nicolaef <fnie2119@scs.ubbcluj.ro>
2018-05-21 13:40:50 +0300

    woooo

---
+++ BloodDonation.Data/Models/UserFirebase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodDonation.Data.Models
+{
+    public class UserFirebase
+    {
+        public string ID { get; set; }
+        public string role { get; set; }
+        public bool isApproved { get; set;}
+    }
+}
