3bb8bb9c590d6dff375b89397b620a233e5afd42
CristiNacu <nacu_cristian@yahoo.com>
2018-05-28 14:46:24 +0300

    Fixed logout button and emails are now sent async to te personnel when a request is made

--- BloodDonation/Controllers/DoctorController.cs
+++ BloodDonation/Controllers/DoctorController.cs
@@ -12,6 +12,8 @@ using BloodDonation.Models;
 using Firebase.Auth;
 using System.Text.RegularExpressions;
 using BloodDonation.Data.Models;
+using System.Net.Mail;
+using System.Threading;
 
 namespace BloodDonation.Controllers
 {
@@ -23,7 +25,7 @@ namespace BloodDonation.Controllers
         private PresentationToBusinessMapperDoctor presentationToBusinessMapperDoctor = new PresentationToBusinessMapperDoctor();
         private PresentationToBusinessMapperPersonnel presentationToBusinessMapperPersonnel = new PresentationToBusinessMapperPersonnel();
 
-        
+
         private RequestService requestService = new RequestService();
 
         List<Models.RequestPersonnel> requests;
@@ -42,7 +44,7 @@ namespace BloodDonation.Controllers
                         .Select(el => businessToPresentationMapperPersonnel.Request(el))
                         .ToList();
 
-            return View("DoctorShowRequestsView",requests);
+            return View("DoctorShowRequestsView", requests);
         }
 
         public ActionResult GetMakeBloodRequest()
@@ -58,6 +60,43 @@ namespace BloodDonation.Controllers
             return MainDoctorPage();
         }
 
+
+
+
+        private void SendBloodRequestMail(Logic.Models.RequestPersonnel newrequest, String destinationEmail)
+        {
+            EmailServiceBloodDonation emailService = new EmailServiceBloodDonation();
+            DoctorService doctorService = new DoctorService();
+            HospitalService hospitalService = new HospitalService();
+
+            Doctor doctorInfo = doctorService.findById(newrequest.doctorId);
+            HospitalTransferObject hospital = hospitalService.GetHospitalById(newrequest.destination);
+            if (doctorInfo != null)
+            {
+                String text = "A blood request has been made by " + "Dr. " + doctorInfo.firstName + " " + doctorInfo.lastName + "\n";
+                text += "The required blood is :\n\tGroup:\t" + newrequest.bloodType.Group + "\n\tRh:\t" + (newrequest.bloodType.RH == true ? "POSITIVE" : "NEGATIVE") + "\n\tQuantity:\t" + newrequest.quantity + "\n";
+                text += "The hospital where the blood is needed:\n\tName:\t" + hospital.Name + "\n\tLocation:\t" + hospital.Location + "\n";
+                text += "Please check the 'BloodDonation' application";
+
+                String title = "Blood Request At " + hospital.Name + " Hospital In " + hospital.Location;
+
+
+                emailService.SendEmail(text, title, doctorInfo.firstName + " " + doctorInfo.lastName, destinationEmail);
+            }
+
+
+        }
+        
+        private void sendEmailToAllPersonnel(Logic.Models.RequestPersonnel newrequest)
+        {
+            PersonnelService personnelService = new PersonnelService();
+            personnelService.FindAll()
+                            .ForEach(staff => SendBloodRequestMail(newrequest, staff.emailAddress));
+
+
+        }
+
+
         [HttpPost]
         public ActionResult CreateRequest(RequestBloodForm request)
         {
@@ -71,6 +110,10 @@ namespace BloodDonation.Controllers
             Logic.Models.RequestPersonnel newRequest = 
                 businessToPresentationMapperDoctor.MapRequestBloodFormToRequestPersonnel(request, GetUid());
             requestService.AddRequest(newRequest);
+
+            /// Send mail async
+            Thread mailThread = new Thread(() => sendEmailToAllPersonnel(newRequest));
+            mailThread.Start();
 
             System.Threading.Thread.Sleep(700);
             return MainDoctorPage();
